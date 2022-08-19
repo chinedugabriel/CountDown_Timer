282310c1cf4012c049e984282753db6244bd4eda
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-19 23:20:51 +0100

    added required veriables in javascript

---
+++ timer.js
@@ -0,0 +1,15 @@
+// view-timer-section veriable 
+let viewTimerSection = document.getElementById("view-timer-section");
+let timeIn_hrs = document.getElementById("hrs");
+let timeIn_min = document.getElementById("min");
+let timeIn_sec = document.getElementById("sec");
+let btnStart = document.getElementById("btn-start");
+let btnStop = document.getElementById("btn-stop");
+
+// set-timer-section veriable
+let setTimerSection = document.getElementById("set-timer-section");
+let inputIn_hours = document.getElementById("input-hour");
+let inputIn_minute = document.getElementById("input-minute");
+let inputIn_seconds = document.getElementById("input-seconds");
+
+let btnSetTimer = document.getElementById("btn-set");
