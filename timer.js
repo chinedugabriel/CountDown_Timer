ed14fb29b15fc38c963ab83ff4268e5e4db26dba
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-20 20:55:27 +0100

    added a button to return to the set timer section and a function to toggle between the sections

--- timer.js
+++ timer.js
@@ -5,6 +5,7 @@ let timeIn_min = document.getElementById("min");
 let timeIn_sec = document.getElementById("sec");
 let btnStart = document.getElementById("btn-start");
 let btnStop = document.getElementById("btn-stop");
+let btnBackToSetTimer = document.getElementById("btn-back-set");
 
 // set-timer-section veriable
 let setTimerSection = document.getElementById("set-timer-section");
@@ -13,3 +14,22 @@ let inputIn_minute = document.getElementById("input-minute");
 let inputIn_seconds = document.getElementById("input-seconds");
 
 let btnSetTimer = document.getElementById("btn-set");
+
+let setTimerHeadingText = document.getElementById("h1-timer");
+
+function toggleSection(){
+    if(setTimerSection.hasAttribute("name")){
+        viewTimerSection.style.display = "flex";
+        setTimerSection.style.display = "none";
+        setTimerSection.removeAttribute("name");
+    }else{
+        viewTimerSection.style.display = "none";
+        setTimerSection.style.display = "flex";
+        setTimerSection.setAttribute("name", "true");
+        
+    }
+}
+
+btnSetTimer.addEventListener("click", toggleSection);
+
+btnBackToSetTimer.addEventListener("click",toggleSection);
